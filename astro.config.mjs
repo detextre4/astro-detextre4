b52c04ca204daee2723f6c53a45cc0eff685b660
detextre4 <detextre4@gmail.com>
2023-09-12 17:27:04 -0400

    testings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    vue({
+      appEntrypoint: '/src/_app',
+      template: {
+        compilerOptions: {
+          // treat any tag that starts with ion- as custom elements
+          isCustomElement: (tag) => tag.startsWith('ion-'),
+        },
+      },
+      // ...
+    })
+  ]
+});
---
+++ src/components/vue-component.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const text = ref('detextre4')
+</script>
